0198b53ea996e32e1c44bc6330220a157f909689
Benny <benny.van.meerbeeck@student.howest.be>
2022-11-16 10:55:16 +0100

    Create throw entity

---
+++ Pin.CricketDarts/Pin.CricketDarts.Core/Entities/Throw.cs
@@ -0,0 +1,15 @@
+﻿namespace Pin.CricketDarts.Core.Entities
+{
+    public class Throw : BaseEntitie
+    {        
+        public byte Fifteen { get; set; }
+        public byte Sixteen { get; set; }
+        public byte Seventeen { get; set; }
+        public byte Eighteen { get; set; }
+        public byte Nineteen { get; set; }
+        public byte Twenty { get; set; }
+        public byte Bull { get; set; }
+
+        public List<PlayerThrow> PlayerThrows { get; set; }
+    }
+}
