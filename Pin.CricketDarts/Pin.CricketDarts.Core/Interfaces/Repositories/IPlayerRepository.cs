a852da99159e45afaf51c65e2d70b284935a3dfc
Benny <benny.van.meerbeeck@student.howest.be>
2022-11-16 13:27:06 +0100

    Create player repository

---
+++ Pin.CricketDarts/Pin.CricketDarts.Core/Interfaces/Repositories/IPlayerRepository.cs
@@ -0,0 +1,16 @@
+﻿using Pin.CricketDarts.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.CricketDarts.Core.Interfaces.Repositories
+{
+    public interface IPlayerRepository:IBaseRepository<Player>
+    {
+
+        Task<IEnumerable<Player>> GetByMatchId(Guid matchId);
+                    
+    }
+}
