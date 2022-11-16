c81e83bbfaf308658dffd31459e0ce44d96f85af
Benny <benny.van.meerbeeck@student.howest.be>
2022-11-16 13:41:34 +0100

    Create base match repository

---
+++ Pin.CricketDarts/Pin.CricketDarts.Core/Interfaces/Repositories/IMatchRepository.cs
@@ -0,0 +1,13 @@
+﻿using Pin.CricketDarts.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.CricketDarts.Core.Interfaces.Repositories
+{
+    public interface IMatchRepository :IBaseRepository<Match>
+    {
+    }
+}
---
+++ Pin.CricketDarts/Pin.CricketDarts.Infrastructure/Repositories/MatchRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pin.CricketDarts.Core.Entities;
+using Pin.CricketDarts.Core.Interfaces.Repositories;
+using Pin.CricketDarts.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.CricketDarts.Infrastructure.Repositories
+{
+    public class MatchRepository : GenericBaseRepository<Match>,IMatchRepository
+    {
+        public MatchRepository(DbDartsContext context):base(context)
+        {
+
+        }
+
+        public async override Task<IEnumerable<Match>> GetAllAsync()
+        {
+            return await _table.Include(m => m.Players)
+                         .OrderByDescending(m => m.TimeStamp)
+                         .ToListAsync();
+        }
+
+        public async override Task<Match> GetByIdAsync(Guid id)
+        {
+            return await _table.Include(m => m.Players)
+                         .FirstOrDefaultAsync(m => m.Id == id);
+        }
+    }
+}
