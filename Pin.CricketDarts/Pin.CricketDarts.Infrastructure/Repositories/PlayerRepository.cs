8b363e3a7f79a823c3d08435578bf38e7993d059
Benny <benny.van.meerbeeck@student.howest.be>
2022-11-16 13:27:16 +0100

    Create player repository

---
+++ Pin.CricketDarts/Pin.CricketDarts.Infrastructure/Repositories/PlayerRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pin.CricketDarts.Core.Entities;
+using Pin.CricketDarts.Core.Interfaces.Repositories;
+using Pin.CricketDarts.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Pin.CricketDarts.Infrastructure.Repositories
+{
+    public class PlayerRepository : GenericBaseRepository<Player>,IPlayerRepository
+    {
+        public PlayerRepository(DbDartsContext context):base(context)
+        {
+
+        }
+        public async override Task<IEnumerable<Player>> GetAllAsync()
+        {
+            return await _table
+                        .Include(p => p.Matches)
+                        .Include(p => p.AllThrows)                     
+                        .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Player>> GetByMatchId(Guid matchId)
+        {
+            return  await _table
+                        .Include(p => p.Matches)
+                        .Where(p => p.Matches.Select(m => m.Id).Contains(matchId))                               
+                        .ToListAsync();                       
+                              
+        }
+    }
+}
