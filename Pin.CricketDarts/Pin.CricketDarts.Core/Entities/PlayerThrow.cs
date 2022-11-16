19e4d9aa9232d46b7fc2f7da8f5ee264ef20cc9b
Benny <benny.van.meerbeeck@student.howest.be>
2022-11-16 10:55:08 +0100

    Create player throw entity

---
+++ Pin.CricketDarts/Pin.CricketDarts.Core/Entities/PlayerThrow.cs
@@ -0,0 +1,13 @@
+﻿namespace Pin.CricketDarts.Core.Entities
+{
+    public class PlayerThrow:BaseEntitie
+    {
+        public Guid MatchId { get; set; }
+        public Match Match { get; set; }
+        public Guid PlayerId { get; set; }
+        public Player Player { get; set; }
+        public Guid ThrowId { get; set; }
+        public Throw Throws { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
