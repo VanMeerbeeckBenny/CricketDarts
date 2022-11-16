c160ae3b99ce7b819059f5d1618b351471abfa5b
Benny <benny.van.meerbeeck@student.howest.be>
2022-11-16 10:54:47 +0100

    Create match entity

---
+++ Pin.CricketDarts/Pin.CricketDarts.Core/Entities/Match.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.CricketDarts.Core.Entities
+{
+    public class Match : BaseEntitie
+    {    
+        public int PlayerOneScore { get; set; }
+        public int PlayerTwoScore { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public bool IsActiveGame { get; set; }
+        public List<Player> Players { get; set; }
+    }
+}
