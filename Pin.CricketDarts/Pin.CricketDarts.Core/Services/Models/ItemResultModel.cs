7e43d0894b0501cfc5b407971f07855308dbd173
Benny <benny.van.meerbeeck@student.howest.be>
2022-11-17 14:29:47 +0100

    Create item result model

---
+++ Pin.CricketDarts/Pin.CricketDarts.Core/Services/Models/ItemResultModel.cs
@@ -0,0 +1,16 @@
+﻿using Pin.CricketDarts.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.CricketDarts.Core.Services.Models
+{
+    public class ItemResultModel<T> where T : BaseEntitie
+    {
+        public bool IsSucces { get; set; }
+        public string ErrorMessage { get; set; }
+        public IEnumerable<T> Items { get; set; }
+    }
+}
