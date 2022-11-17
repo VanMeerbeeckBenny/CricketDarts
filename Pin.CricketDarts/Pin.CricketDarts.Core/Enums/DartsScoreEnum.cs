4112be88872904c40490017247a65ff9f72348d6
Benny <benny.van.meerbeeck@student.howest.be>
2022-11-17 14:15:41 +0100

    Create darts score enum

---
+++ Pin.CricketDarts/Pin.CricketDarts.Core/Enums/DartsScoreEnum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.CricketDarts.Core.Enums
+{
+    public enum DartsScoreEnum
+    {
+        Fifteen = 15,
+        DoubleFifteen = 30,
+        TripleFifteen = 45,
+        Sixteen = 16,
+        DoubleSixteen = 32,
+        TripleSixteen = 48,
+        Seventeen = 17,
+        DoubleSeventeen = 34,
+        TripleSeventeen = 51,
+        Eighteen = 18,
+        DoubleEighteen = 36,
+        TripleEigteen = 54,
+        NineTeen = 19,
+        DoubleNineTeen = 38,
+        TripleNineTeen = 57,
+        Twenty = 20,
+        DoubleTwenty = 40,
+        TripleTwenty = 60,
+        BullOuter = 25,
+        BullInner = 50
+    }
+}
