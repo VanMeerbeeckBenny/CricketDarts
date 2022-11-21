b676f037ad26506b4cb0f7cbb034940f09a7873e
Benny <benny.van.meerbeeck@student.howest.be>
2022-11-21 11:38:48 +0100

    Create darts player model

---
+++ Pin.CricketDarts/Pin.CricketDarts.Web/Models/DartsPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pin.CricketDarts.Web.Models
+{
+    public class DartsPlayer : BaseModel
+    {
+        [Required(ErrorMessage = "Please provide a {0}")]
+        [MinLength(2,ErrorMessage ="{0} must have a length of minimum {1}")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please provide a {0}")]
+        [MinLength(2, ErrorMessage = "{0} must have a length of minimum {1}")]
+        public string LastName { get; set; }
+        public int Score { get; set; }
+        public List<ThrowModel> Throws { get; set; }
+        public bool IsActiveTurn { get; set; }
+    }
+}
+
