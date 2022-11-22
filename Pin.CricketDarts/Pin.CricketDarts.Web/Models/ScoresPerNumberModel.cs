ab01893c17eb1d16cf478bfa3617a9ed3d7185ce
Benny <benny.van.meerbeeck@student.howest.be>
2022-11-22 12:37:00 +0100

    Create score per number model

---
+++ Pin.CricketDarts/Pin.CricketDarts.Web/Models/ScoresPerNumberModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Pin.CricketDarts.Web.Models
+{
+    public class ScoresPerNumberModel
+    {
+        public string Name { get; set; }    
+        public ScoreModel Single { get; set; }     
+        public ScoreModel Double { get; set; }
+        public ScoreModel Triple { get; set; }
+    }
+}
