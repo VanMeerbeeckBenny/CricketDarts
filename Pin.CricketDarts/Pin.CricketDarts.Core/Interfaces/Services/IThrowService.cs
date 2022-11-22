2fb66ead8f5c2298a00d4ef30036f1d98c6e0c83
Benny <benny.van.meerbeeck@student.howest.be>
2022-11-22 12:34:51 +0100

    Create throw service

---
+++ Pin.CricketDarts/Pin.CricketDarts.Core/Interfaces/Services/IThrowService.cs
@@ -0,0 +1,16 @@
+﻿using Pin.CricketDarts.Core.Entities;
+using Pin.CricketDarts.Core.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.CricketDarts.Core.Interfaces.Services
+{
+    public interface IThrowService
+    {
+        Task <ItemResultModel<Point>> GetThrowByValue(int value);
+        Task<ItemResultModel<Point>> GetAll();
+    }
+}
---
+++ Pin.CricketDarts/Pin.CricketDarts.Core/Services/ThrowService.cs
@@ -0,0 +1,54 @@
+﻿using Pin.CricketDarts.Core.Entities;
+using Pin.CricketDarts.Core.Interfaces.Repositories;
+using Pin.CricketDarts.Core.Interfaces.Services;
+using Pin.CricketDarts.Core.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.CricketDarts.Core.Services
+{
+    public class ThrowService:IThrowService
+    {
+        private readonly IThrowRepository _throwRepository;
+
+        public ThrowService(IThrowRepository throwRepository)
+        {
+            _throwRepository = throwRepository;
+        }
+
+        public async Task<ItemResultModel<Point>> GetThrowByValue(int value)
+        {
+            var throwByValue = await _throwRepository.GetThrowByValue(value);
+
+            if (throwByValue == null) return new ItemResultModel<Point>
+            {
+                ErrorMessage = "An error hase accures, please try again later!"
+            };
+
+            return new ItemResultModel<Point>
+            {
+                Items =  new List<Point> { throwByValue },
+                IsSucces = true
+            };
+        }
+
+        public async Task<ItemResultModel<Point>> GetAll()
+        {
+            var allScores = await _throwRepository.GetAll();
+
+            if (allScores == null || !allScores.Any()) return new ItemResultModel<Point>
+            {
+                ErrorMessage = "An error hase accures, please try again later!"
+            };
+
+            return new ItemResultModel<Point>
+            {
+                Items = allScores ,
+                IsSucces = true
+            };
+        }
+    }
+}
