10c26fe5f67449ebdd412d3f5fd65898aae8e7a1
Benny <benny.van.meerbeeck@student.howest.be>
2022-11-22 15:36:59 +0100

    Create throw model

---
+++ Pin.CricketDarts/Pin.CricketDarts.Web/Models/ThrowModel.cs
@@ -0,0 +1,10 @@
+﻿using Pin.CricketDarts.Core.Enums;
+
+namespace Pin.CricketDarts.Web.Models
+{
+    public class ThrowModel
+    {
+        public DartsScoreEnum Throw { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
