386ff6a6d0b470e637b5a4aed0e2547b1d142f51
Benny <benny.van.meerbeeck@student.howest.be>
2022-11-24 22:15:49 +0100

    Create finisched numbers model

---
+++ Pin.CricketDarts/Pin.CricketDarts.Web/Models/FinischedNumbersModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Pin.CricketDarts.Web.Models
+{
+    public class FinischedNumbersModel
+    {
+        public bool Fifteen { get; set; }
+        public bool Sixteen { get; set; }
+        public bool Seventeen { get; set; }
+        public bool Eighteen { get; set; }
+        public bool NineTeen { get; set; }
+        public bool Twenty { get; set; }
+        public bool Bull { get; set; }
+
+    }
+}
