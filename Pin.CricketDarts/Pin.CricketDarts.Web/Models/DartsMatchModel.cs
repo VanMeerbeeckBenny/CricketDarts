6ccbc56ed6c08bc10d3ca3ef109b479dae7728b6
Benny <benny.van.meerbeeck@student.howest.be>
2022-11-25 14:03:43 +0100

    Add id

--- Pin.CricketDarts/Pin.CricketDarts.Web/Models/DartsMatchModel.cs
+++ Pin.CricketDarts/Pin.CricketDarts.Web/Models/DartsMatchModel.cs
@@ -2,6 +2,7 @@
 {
     public class DartsMatchModel
     {
+        public Guid Id { get; set; }
         public DartsPlayerModel PlayerOne { get; set; }
         public DartsPlayerModel PlayerTwo { get; set; }
         public bool IsActiveMatch { get; set; }
