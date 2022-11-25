8e6bc4bf319e03119f61719452140a3dea547e51
Benny <benny.van.meerbeeck@student.howest.be>
2022-11-25 16:12:37 +0100

    Create interface player throw service

---
+++ Pin.CricketDarts/Pin.CricketDarts.Core/Interfaces/Services/IPlayerThrowService.cs
@@ -0,0 +1,18 @@
+﻿using Pin.CricketDarts.Core.Entities;
+using Pin.CricketDarts.Core.Interfaces.Repositories;
+using Pin.CricketDarts.Core.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.CricketDarts.Core.Interfaces.Services
+{
+    public interface IPlayerThrowService
+    {      
+        
+        Task<ItemResultModel<PlayerThrow>> GetByUserAndMatchIdAsync(Guid matchId, Guid playerId);
+        Task<ItemResultModel<PlayerThrow>> CreateAsync(Guid matchId, Guid playerId, Guid throwId);
+    }
+}
