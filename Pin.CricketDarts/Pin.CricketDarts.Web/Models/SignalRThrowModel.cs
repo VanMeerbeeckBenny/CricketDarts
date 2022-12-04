2ea185b6d4df96c31a7b7b1e541a92b8dad9cb27
Benny <benny.van.meerbeeck@student.howest.be>
2022-12-04 17:35:17 +0100

    Create signal r throw model

---
+++ Pin.CricketDarts/Pin.CricketDarts.Web/Models/SignalRThrowModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Pin.CricketDarts.Web.Models
+{
+    public class SignalRThrowModel
+    {
+        public ThrowModel Throw { get; set; }
+        public Guid MatchId { get; set; }
+        public Guid PlayerId { get; set; }
+        public int CurrentScore { get; set; }
+        public bool IsDone { get; set; }       
+    }
+}
