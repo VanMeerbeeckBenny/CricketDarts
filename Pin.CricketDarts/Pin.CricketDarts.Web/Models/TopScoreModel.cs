05c2d374284a569b195e59e6b652b00e133b1c11
Benny <benny.van.meerbeeck@student.howest.be>
2022-12-04 20:48:52 +0100

    Create top scoremodel

---
+++ Pin.CricketDarts/Pin.CricketDarts.Web/Models/TopScoreModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Pin.CricketDarts.Web.Models
+{
+    public class TopScoreModel
+    {
+        public Guid PlayerId { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int CountOfThrows { get; set; }
+    }
+}
