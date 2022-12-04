d3036c2a51ba05d4b01337ee8f7a425f97f805bf
Benny <benny.van.meerbeeck@student.howest.be>
2022-12-04 13:56:57 +0100

    Create match player repository

---
+++ Pin.CricketDarts/Pin.CricketDarts.Core/Interfaces/Repositories/IMatchPlayerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Pin.CricketDarts.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.CricketDarts.Core.Interfaces.Repositories
+{
+    public interface IMatchPlayerRepository:IBaseRepository<MatchPlayer>
+    {
+        Task<MatchPlayer> GetByMatchAndPlayerId(Guid matchId, Guid playerId);
+    }
+}
---
+++ Pin.CricketDarts/Pin.CricketDarts.Infrastructure/Repositories/MatchPlayerRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pin.CricketDarts.Core.Entities;
+using Pin.CricketDarts.Core.Interfaces.Repositories;
+using Pin.CricketDarts.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.CricketDarts.Infrastructure.Repositories
+{
+    public class MatchPlayerRepository:GenericBaseRepository<MatchPlayer>,IMatchPlayerRepository
+    {
+        public MatchPlayerRepository(DbDartsContext dbContext):base(dbContext)
+        {
+                
+        }
+
+        public async Task<MatchPlayer> GetByMatchAndPlayerId(Guid matchId,Guid playerId)
+        {
+            return await _table
+                .Include(mp => mp.Player)
+                .Include(mp => mp.Match)
+                .FirstOrDefaultAsync(mp => mp.PlayerId == playerId && mp.MatchId == matchId);
+                               
+        }
+    }
+}
