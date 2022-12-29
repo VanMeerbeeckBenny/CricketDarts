5d3ed1c1decb8e20e01fabe52ad6110be5e4bcbc
Benny <benny.van.meerbeeck@student.howest.be>
2022-12-29 01:15:45 +0100

    Create dartsplayerwin

---
+++ Pin.CricketDarts/Pin.CricketDarts.Web/Models/DartsPlayerWin.cs
@@ -0,0 +1,9 @@
+﻿namespace Pin.CricketDarts.Web.Models
+{
+    public class DartsPlayerWin : BaseModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int WonMatches { get; set; }
+    }
+}
