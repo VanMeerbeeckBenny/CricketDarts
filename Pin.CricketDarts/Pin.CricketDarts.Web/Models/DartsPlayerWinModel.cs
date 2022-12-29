3cfb19ead76cb8ea096e5d2ff524c27e87067e3c
Benny <benny.van.meerbeeck@student.howest.be>
2022-12-29 17:03:41 +0100

    Change name

--- Pin.CricketDarts/Pin.CricketDarts.Web/Models/DartsPlayerWinModel.cs
+++ Pin.CricketDarts/Pin.CricketDarts.Web/Models/DartsPlayerWinModel.cs
@@ -1,9 +1,11 @@
 ﻿namespace Pin.CricketDarts.Web.Models
 {
-    public class DartsPlayerWin : BaseModel
+    public class DartsPlayerWinModel : BaseModel
     {
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public int WonMatches { get; set; }
+        public int AverageScore { get; set; }
     }
 }
+
