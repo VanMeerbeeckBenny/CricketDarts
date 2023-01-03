e62b433ba11f1b8192a861ac90c3787d8ff445f7
Benny <benny.van.meerbeeck@student.howest.be>
2023-01-03 18:30:46 +0100

    Add properties

--- Pin.CricketDarts/Pin.CricketDarts.Web/Models/ThrowModel.cs
+++ Pin.CricketDarts/Pin.CricketDarts.Web/Models/ThrowModel.cs
@@ -2,10 +2,11 @@
 
 namespace Pin.CricketDarts.Web.Models
 {
-    public class ThrowModel
-    {
+    public class ThrowModel : BaseModel
+    {       
         public int BaseNumber { get; set; }
         public int Throw { get; set; }
         public DateTime TimeStamp { get; set; }
+        public Guid MatchId { get; set; }
     }
 }
